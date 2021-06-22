f21eaa5ed20c27a0c0972272de9419ee00ee877a
TheAnhPhamIT <theanhphamit@gmail.com>
2021-06-22 14:42:29 +0700

    modify sticky-navbar project

--- sticky-navbar/script.js
+++ sticky-navbar/script.js
@@ -1,9 +1,9 @@
 const nav = document.querySelector('.nav');
 
-document.addEventListener('scroll', fixNav);
+window.addEventListener('scroll', fixNav);
 
 function fixNav() {
-    if(document.scrollY > nav.offsetHeight + 150) {
+    if(window.scrollY > nav.offsetHeight + 150) {
         nav.classList.add('active');
     } else {
         nav.classList.remove('active');
